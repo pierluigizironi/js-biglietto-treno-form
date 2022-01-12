682e9b014c421f1d2c39ba49d03ea58114e4cf88
pierluigizironi <zironipierluigi@gmail.com>
2022-01-12 15:37:10 +0100

    add input for the fullname

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('JS OK')
+
+const fullName = document.getElementById('fullname');
+const buttonElement = document.querySelector('button');
+const inputElement = document.querySelector('input');
+
+buttonElement.addEventListener('click', function () {
+
+    const inputValue = inputElement.value; 
+    console.log(inputValue);
+    
+    if (inputValue) {
+    fullName.innerHTML = `Ciao, <strong>${inputValue}</strong>`;
+    }
+    });
